576f110a2aa2b18c7def23bcd7eb40abee4545dc
Alexander Sumkin <a-sumkin@yandex.ru>
2024-10-21 18:59:47 +0300

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+/*
+This program code created my MS and modified by Aleksandr Sumkin
+for education purposes only.
+*/
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- obj/Debug/net8.0/myApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0e2ee4879d8621e461562131272125870af425")]
 [assembly: System.Reflection.AssemblyProductAttribute("myApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
